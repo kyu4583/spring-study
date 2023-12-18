f42bbfdbe2a19c25f7bcbaf6edde623371935fe4
Sungkyu <kxu4583@naver.com>
2023-12-18 20:55:09 +0900

    feat: API 방식 컨트롤러 구현
    '@RequestBody'를 사용함.

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -17,5 +18,11 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model){
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody //(html 바디 말고)http의 바디에 리턴값을 그대로 넣겠다.
+    public String helloString(@RequestParam("name") String name){
+        return "hello " + name;
     }
 }
