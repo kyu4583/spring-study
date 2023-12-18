edd507a34eb1fc3c74afea31bbc98cb9374dcf2c
Sungkyu <kxu4583@naver.com>
2023-12-18 22:44:20 +0900

    feat: 객체 반환 API 컨트롤러 구현

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -24,5 +25,25 @@ public class HelloController {
     @ResponseBody //(html 바디 말고)http의 바디에 리턴값을 그대로 넣겠다.
     public String helloString(@RequestParam("name") String name){
         return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName("sungkyu");
+        return hello;
+    }
+
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
