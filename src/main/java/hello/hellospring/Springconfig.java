dedd1b3c0175fe2583ff613471017fb478bb78d4
Sungkyu <kxu4583@naver.com>
2023-12-21 01:33:12 +0900

    feat: 스프링 데이터 JPA 적용

--- src/main/java/hello/hellospring/Springconfig.java
+++ src/main/java/hello/hellospring/Springconfig.java
@@ -2,7 +2,6 @@ package hello.hellospring;
 
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,22 +15,29 @@ public class Springconfig {
 //        this.dataSource = dataSource;
 //    }
 
-    private EntityManager em;
+//    private EntityManager em;
+//
+//    @Autowired
+//    public Springconfig(EntityManager em) {
+//        this.em = em;
+//    }
+
+    private final MemberRepository memberRepository;
 
     @Autowired
-    public Springconfig(EntityManager em) {
-        this.em = em;
+    public Springconfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-        //return new JdbcMemberRepository(dataSource);
-        //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        //return new JdbcMemberRepository(dataSource);
+//        //return new JdbcTemplateMemberRepository(dataSource);
+//        return new JpaMemberRepository(em);
+//    }
 }
