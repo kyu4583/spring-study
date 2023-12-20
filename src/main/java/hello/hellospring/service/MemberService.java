2f4cdd7f6fb9b85f7781df7cb86c2315b257d6e9
Sungkyu <kxu4583@naver.com>
2023-12-20 14:34:50 +0900

    feat: 의존성 주입 적용

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,13 +3,17 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+
 public class MemberService {
     private final MemberRepository memberRepository;
     //디펜던시 인젝션, 테스트 등 다른 환경에서도 같은 인스턴스를 가지고 동작하게 해줌
+    @Autowired //의존성 주입
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
